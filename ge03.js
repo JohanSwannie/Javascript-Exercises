2e7dc5a1d5dc5ed719657a7820e614579bcf6dc0
Johan Swan <jswannieman@gmail.com>
2023-11-24 20:34:05 +1300

    Update ge03 JS Component

--- ge03.js
+++ ge03.js
@@ -1,24 +1,35 @@
 let isOver = false;
 let numArray = [];
+let duplicates = 0;
+let numbersGenerated = 0;
 
 while (!isOver) {
-  randy = Math.floor(Math.random() * 40) + 1;
-  if (!numArray.includes(randy)) {
-    numArray.push(randy);
-  }
   let continueLoop = confirm(
-    `Valid random number added if no duplicates was found - Continue the loop?`
+    `A valid random number will be added if no duplicates is found - do you want to continue?`
   );
   isOver = continueLoop === true ? false : true;
+  if (!isOver) {
+    randy = Math.floor(Math.random() * 40) + 1;
+    if (!numArray.includes(randy)) {
+      numArray.push(randy);
+      numbersGenerated++;
+    } else {
+      duplicates++;
+    }
+  }
 }
 
 alert(
   "You reached the end of your random number search! See your list of numbers below"
 );
 
-document.getElementById(
-  "numberList"
-).innerHTML = `Your number list = ${numArray}`;
+const numberList = document.getElementById("numberList");
+
+if (numArray.length === 0) {
+  numberList.innerHTML = `Your number list is empty`;
+} else {
+  numberList.innerHTML = `Your number list is ${numArray} - during generation ${duplicates} duplicates were found and ${numbersGenerated} numbers were generated`;
+}
 
 const butter = document.getElementById("butty");
 
