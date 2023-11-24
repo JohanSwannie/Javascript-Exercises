9aa8714a047cf061b8265b0d42465dbabd292c61
Johan Swan <jswannieman@gmail.com>
2023-11-24 15:29:04 +1300

    Update ge01 JS Component

--- ge01.js
+++ ge01.js
@@ -1,27 +1,30 @@
-let firstNumber = Number(prompt("Enter first number"));
-let secondNumber = Number(prompt("Enter second number"));
-let operand = prompt("Enter operand (+, -, * or /)");
-let result;
+let indicator = " ";
 
-if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
-  switch (operand) {
-    case "+":
-      result = firstNumber + secondNumber;
-      break;
-    case "-":
-      result = firstNumber - secondNumber;
-      break;
-    case "*":
-      result = firstNumber * secondNumber;
-      break;
-    case "/":
-      result = firstNumber / secondNumber;
-      break;
-    default:
-      result = "Error: unknown operand";
+while (indicator !== "q") {
+  let firstNumber = Number(prompt("Enter first number"));
+  let secondNumber = Number(prompt("Enter second number"));
+  let operand = prompt("Enter operand (+, -, * or /)");
+  let result = 0;
+  if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+    switch (operand) {
+      case "+":
+        result = firstNumber + secondNumber;
+        break;
+      case "-":
+        result = firstNumber - secondNumber;
+        break;
+      case "*":
+        result = firstNumber * secondNumber;
+        break;
+      case "/":
+        result = firstNumber / secondNumber;
+        break;
+      default:
+        result = "Error: unknown operand";
+    }
+  } else {
+    result = "Error: at least one of the entered values is not a number";
   }
-} else {
-  result = "Error: at least one of the entered values is not a number";
+  alert(`Answer is ${result}`);
+  indicator = prompt(`Click OK to play again or enter 'q' to quit`);
 }
-
-alert(result);
