3b1f09646a078669498684a4b69bec443eb5c98b
Johan Swan <jswannieman@gmail.com>
2023-11-25 19:40:27 +1300

    Update ge12 JS Component

--- ge12.js
+++ ge12.js
@@ -1,6 +1,7 @@
 let a = true && 20;
 let b = 0 || 20;
 let c = 0 && 20;
+
 console.log(`${a} ${b} ${c}`);
 
 let a2 = 20 + "10";
@@ -8,11 +9,14 @@ let b2 = 20 + +"10";
 let c2 = 20 + -"10" + "10";
 let d2 = "10" - "10" + "100";
 let e2 = "A" - "B" + 0xa;
+
 console.log(`${a2}, ${b2}, ${c2}, ${d2}, ${e2}`);
 
-let x = [10, 20, 30, 40];
+let x = [10, 20, 30, 40, 50];
 let y = [50, 60];
-x.reverse().push(y);
+
+x.reverse().push(y[0]);
+
 console.log(x.length);
 
 for (let a3 = 1; a3 < 10; a3 += 2) {
@@ -20,17 +24,19 @@ for (let a3 = 1; a3 < 10; a3 += 2) {
 }
 
 let counter = 0;
-while (counter++ < 10) console.log(counter++);
+while (counter++ < 10) console.log("Our Counter is " + counter++);
 
 let route = { distance: 131, elevation: 1.4 };
-for (let k in route) console.log(k);
+for (let k in route) console.log(`${k} : ${route[k]}`);
 
 let a4 = (n) => {
   return n > 2 ? n * a4(n - 1) : 2;
 };
-console.log(a4(6));
+
+console.log(a4(7));
 
 let counter2 = 2;
+
 let interval = setInterval(() => {
   console.log(counter2);
   if (counter2-- <= 0) clearInterval(interval);
