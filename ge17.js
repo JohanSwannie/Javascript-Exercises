22a0df2d3c2ba39a1efc088db89371db821d829e
Johan Swan <jswannieman@gmail.com>
2024-03-05 07:59:54 +1300

    Update ge17 JS Component

--- ge17.js
+++ ge17.js
@@ -13,9 +13,11 @@ console.log(operation(doIt, 12, 10));
 let numbers1 = [100, 5, 80, 55, 70, 50, 10, 40, 30, 20];
 let numbers2 = [9, 38, 14, 20, 21, 6, 18, 4];
 let numbers3 = [17, 10, 19, 2, 28, 30, 5];
+let letters1 = ["J", "A", "P", "E", "D", "Z", "V", "X"];
 
-const compareNumbers = (a, b) => (a > b ? 1 : -1);
+const sortThem = (a, b) => (a > b ? 1 : -1);
 
-console.log(`numbers1 sorted are : ${numbers1.sort(compareNumbers)}`);
-console.log(`numbers2 sorted are : ${numbers2.sort(compareNumbers)}`);
-console.log(`numbers3 sorted are : ${numbers3.sort(compareNumbers)}`);
+console.log(`numbers1 sorted are : ${numbers1.sort(sortThem)}`);
+console.log(`numbers2 sorted are : ${numbers2.sort(sortThem)}`);
+console.log(`numbers3 sorted are : ${numbers3.sort(sortThem)}`);
+console.log(`letters1 sorted are : ${letters1.sort(sortThem)}`);
