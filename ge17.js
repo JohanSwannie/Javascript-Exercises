407ecf2d13292fc3edcdffde2e43abc93c144016
Johan Swan <jswannieman@gmail.com>
2024-03-05 07:49:54 +1300

    Update ge17 JS Component

--- ge17.js
+++ ge17.js
@@ -10,26 +10,12 @@ console.log(operation(doIt, 12, 10));
 
 // --------------------------------------------
 
-const inner3 = () => {
-  console.log("inner3 1");
-};
+let numbers1 = [100, 5, 80, 55, 70, 50, 10, 40, 30, 20];
+let numbers2 = [9, 38, 14, 20, 21, 6, 18, 4];
+let numbers3 = [17, 10, 19, 2, 28, 30, 5];
 
-let outer3 = (callback3) => {
-  console.log("outer3 1");
-  let counter = 1;
-  let timerId = setInterval(() => {
-    console.log(`outer3 2 with counter : ${counter}`);
-    counter++;
-    callback3();
-  }, 2000);
-  setTimeout(() => {
-    console.log("TIMER ID INTERVAL HAS BEEN CANCELLED");
-    clearInterval(timerId);
-  }, 20000);
-};
+const compareNumbers = (a, b) => (a > b ? 1 : -1);
 
-console.log("test3 1");
-
-outer3(inner3);
-
-console.log("test3 2");
+console.log(`The numbers1 sorted are now : ${numbers1.sort(compareNumbers)}`);
+console.log(`The numbers2 sorted are now : ${numbers2.sort(compareNumbers)}`);
+console.log(`The numbers3 sorted are now : ${numbers3.sort(compareNumbers)}`);
