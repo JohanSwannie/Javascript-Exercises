f99825c3a53185da9211966c7761f69766df2488
Johan Swan <jswannieman@gmail.com>
2024-03-11 08:57:19 +1300

    Update ge20 JS Component

--- ge20.js
+++ ge20.js
@@ -1,29 +1,14 @@
-var largestNumber = [];
+const determineLargest = (array) => {
+  return array.map((arr) => Math.max.apply(null, arr));
+};
 
-function largestNumbersInArrays1(array) {
-  for (let p = 0; p < array.length; p++) {
-    largestNumber.push(0);
-  }
-  for (var q = 0; q < array.length; q++) {
-    for (var r = 0; r < array[q].length; r++) {
-      if (array[q][r] > largestNumber[q]) {
-        largestNumber[q] = array[q][r];
-      }
-    }
-  }
-  return largestNumber;
-}
-
-const array10 = [
-  [19, 67, 57, 88, 7, 16, 33],
-  [28, 71, 107, 22, 33, 51],
-  [39, 44, 73, 55, 13, 10, 21],
-  [444, 500, 177, 939, 239, 712],
-  [49, 110, 183, 131, 77, 83],
+const array1 = [
+  [27, 19, 39, 7, 11, 47],
+  [14, 41, 35, 28, 20, 91],
+  [31, 22, 50, 16, 12, 30],
+  [51, 60, 23, 64, 33, 47],
 ];
 
-let largestNumbers1 = `The largest number in each array is ${largestNumbersInArrays1(
-  array10
-)}`;
-
-console.log(largestNumbers1);
+console.log(
+  `The largest number in each array is : ${determineLargest(array1)}`
+);
