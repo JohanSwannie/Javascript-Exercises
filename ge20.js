5bbb6f29e46ff0332016d73b420560604a3ded6e
Johan Swan <jswannieman@gmail.com>
2024-03-11 14:04:58 +1300

    Update ge20 JS Component

--- ge20.js
+++ ge20.js
@@ -18,3 +18,9 @@ function removeSmallest(numbers) {
 }
 
 console.log(`New array is : ${removeSmallest([5, 9, 9, 2, 1, 3, 8, 1, 2, 7])}`);
+
+let array7 = [9, 8, 12, 21, 7, 15, 29, 10];
+
+let array8 = array7.slice(0, 4).concat(array7.slice(5, 7));
+
+console.log(array8);
