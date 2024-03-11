b9e344e34b5ba76d8bb6459fcc293590bae13337
Johan Swan <jswannieman@gmail.com>
2024-03-11 14:09:41 +1300

    Update ge20 JS Component

--- ge20.js
+++ ge20.js
@@ -1,26 +1,8 @@
 function removeSmallest(numbers) {
-  let newArray = [];
-  if (numbers.length === 0) {
-    return newArray;
-  }
-  let smallestIndex = 0;
-  let smallestNumber = Infinity;
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < smallestNumber) {
-      smallestNumber = numbers[i];
-      smallestIndex = i;
-    }
-  }
-  newArray = numbers
-    .slice(0, smallestIndex)
-    .concat(numbers.slice(smallestIndex + 1));
-  return newArray;
+  let smallestIdx = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, smallestIdx), ...numbers.slice(smallestIdx + 1)];
 }
 
-console.log(`New array is : ${removeSmallest([5, 9, 9, 2, 1, 3, 8, 1, 2, 7])}`);
-
-let array7 = [9, 8, 12, 21, 7, 15, 29, 10];
-
-let array8 = array7.slice(0, 4).concat(array7.slice(5, 7));
-
-console.log(array8);
+console.log(
+  `New array is : ${removeSmallest([5, 9, -13, 9, 2, -21, 3, 8, 1, 2, 7])}`
+);
