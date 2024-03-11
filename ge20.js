4bfb5030057e73307a67800e2264ee104dd3cff9
Johan Swan <jswannieman@gmail.com>
2024-03-12 11:42:36 +1300

    Update ge20 JS Component

--- ge20.js
+++ ge20.js
@@ -1,12 +1,15 @@
-function truncateTheString(str, num) {
-  if (str.length > num) {
-    return str.slice(11, num) + "...";
-  } else {
-    return str;
+const findElementToBeTrue = (array, func) => {
+  let number = 0;
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    number = array[i];
+    if (func(number) === true) {
+      newArray.push(number);
+    }
   }
-}
+  return newArray;
+};
 
-let stringToTruncate = "Jack runs down the hill at a fast pace";
-let truncateLength = 21;
+let arrayToPass = [13, 3, 9, 15, 14, 10, 19, 35];
 
-console.log(`${truncateTheString(stringToTruncate, truncateLength)}`);
+console.log(`${findElementToBeTrue(arrayToPass, (num) => num % 2 === 0)}`);
